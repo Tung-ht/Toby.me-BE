76071de12249de36ea288a9ea4852d2976f72a0d
Tung Hoang <tung.ht184325@sis.hust.edu.vn>
2023-07-29 03:49:44 +0700

    modify: default avatar url

--- toby-auth/src/main/java/tunght/toby/auth/consts/CommonConst.java
+++ toby-auth/src/main/java/tunght/toby/auth/consts/CommonConst.java
@@ -3,5 +3,5 @@ package tunght.toby.auth.consts;
 public class CommonConst {
     public static final String REGISTRATION_SUCCESS_MSG = "Email của bạn được xác minh thành công, bạn đã có thể đăng nhập";
     public static final String RESET_PASSWORD_SUCCESS_MSG = "Đặt lại mật khẩu thành công, bạn có thể sử dụng mật khẩu mới để đăng nhập";
-    public static final String DEFAULT_AVATAR_URL = "https://png.pngtree.com/element_our/20200610/ourmid/pngtree-character-default-avatar-image_2237203.jpg";
+    public static final String DEFAULT_AVATAR_URL = "https://www.iconpacks.net/icons/1/free-user-icon-295-thumb.png";
 }
