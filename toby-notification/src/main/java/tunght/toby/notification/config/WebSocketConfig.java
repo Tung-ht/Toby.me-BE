943cef7a0c1b5ef2426ddef5bf469e218ecc7706
vietmq <vietmq@viettel.com.vn>
2023-07-31 00:06:00 +0700

    modify: add WebSocketConfigurer

--- toby-notification/src/main/java/tunght/toby/notification/config/WebSocketConfig.java
+++ toby-notification/src/main/java/tunght/toby/notification/config/WebSocketConfig.java
@@ -2,8 +2,22 @@ package tunght.toby.notification.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import tunght.toby.notification.domain.websocket.network.MyWebSocketHandler;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig {
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final MyWebSocketHandler socketHandler;
+
+    public WebSocketConfig(MyWebSocketHandler socketHandler) {
+        this.socketHandler = socketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(socketHandler, "/endpoint").setAllowedOrigins("*");
+    }
 }
